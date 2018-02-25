72e817a5a5fd514e18424c32ecb44a80f9769764
islam abdelmaaboud <islamabdelmaaboud@yahoo.com>
2018-02-25 17:00:09 +0200

    register model to site admin

--- users/admin.py
+++ users/admin.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from models import User
+
+# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    fields = ["first_name","last_name","iban"]
+    list_display = ["first_name","last_name","iban",'created_by']
+    list_filter = ['created_by']
+    readonly_fields = ('created_by', )
+
+admin.site.register(User,UserAdmin)
