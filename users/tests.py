c4525ef9d04657fbd529581dbbf40efc43fea873
islam abdelmaaboud <islamabdelmaaboud@yahoo.com>
2018-02-26 10:56:08 +0200

    test admin can update his objects and not update the objects created by other admins

--- users/tests.py
+++ users/tests.py
@@ -3,22 +3,23 @@ from __future__ import unicode_literals
 
 import time
 from django.test import TestCase,LiveServerTestCase
-
+from models import User
 from selenium import webdriver
 from django.contrib.auth import get_user_model
 # Create your tests here.
 class UserIbanCase(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-
         self.browser.implicitly_wait(1)
-        self.admin_user = get_user_model().objects.create_superuser(username='islam', email='islam@example.com',
+        self.admin_user1 = get_user_model().objects.create_superuser(username='islam', email='islam@example.com',
                                                                     password='password1')
-        self.admin_user = get_user_model().objects.create_superuser(username='ahmed', email='ahmed@example.com',
+        self.admin_user2 = get_user_model().objects.create_superuser(username='ahmed', email='ahmed@example.com',
                                                                     password='password2')
-        self.admin_user = get_user_model().objects.create_superuser(username='ali', email='ali@example.com',
+        self.admin_user3 = get_user_model().objects.create_superuser(username='ali', email='ali@example.com',
                                                                     password='password3')
-
+        User.objects.create(first_name="mohamed",last_name="sayed",iban="123456789",created_by= self.admin_user2)
+        User.objects.create(first_name="ramy",last_name="samir",iban="5678912354",created_by= self.admin_user1)
+        User.objects.create(first_name="hany",last_name="bassem",iban="5678912354",created_by= self.admin_user1)
     # this test Case just try to test admin login
     """
     def test_failed_admin_login(self):
@@ -90,7 +91,73 @@ class UserIbanCase(LiveServerTestCase):
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('was added successfully', body.text)
         self.assertIn('ahmed', body.text)
+    # check can't update users that he didn't add in admin pannel
+    def test_admin_not_update_user(self):
+        home_page = self.browser.get(self.live_server_url + '/admin')
+        user_name_input = self.browser.find_element_by_name("username")
+        password_input = self.browser.find_element_by_name("password")
+        submit = self.browser.find_element_by_xpath("//input[@type='submit']")
+        user_name_input.clear()
+        password_input.clear()
 
+        user_name_input.send_keys("islam")
+        password_input.send_keys("password1")
+        submit.submit()
+
+        users_link = self.browser.find_elements_by_partial_link_text("Users IBAN")
+        users_link[0].click()
+        time.sleep(5)
+        add_user = self.browser.find_element_by_partial_link_text('mohamed')
+        add_user.click()
+
+
+        first_name_input = self.browser.find_element_by_name("first_name")
+        last_name_input = self.browser.find_element_by_name("last_name")
+        iban_input = self.browser.find_element_by_name("iban")
+        first_name_input.clear()
+        first_name_input.send_keys("xxxxx")
+        last_name_input.clear()
+
+        last_name_input.send_keys("xxxxx")
+
+        self.browser.find_element_by_name("_save").submit()
+        time.sleep(1)
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('was changed successfully', body.text)
+
+        self.assertNotIn('xxxxx', body.text)
+
+    # check update users that he added in admin pannel
+    def test_admin_update_user(self):
+        home_page = self.browser.get(self.live_server_url + '/admin')
+        user_name_input = self.browser.find_element_by_name("username")
+        password_input = self.browser.find_element_by_name("password")
+        submit = self.browser.find_element_by_xpath("//input[@type='submit']")
+        user_name_input.clear()
+        password_input.clear()
+
+        user_name_input.send_keys("islam")
+        password_input.send_keys("password1")
+        submit.submit()
+
+        users_link = self.browser.find_elements_by_partial_link_text("Users IBAN")
+        users_link[0].click()
+        add_user = self.browser.find_element_by_partial_link_text('hany')
+        add_user.click()
+
+
+        first_name_input = self.browser.find_element_by_name("first_name")
+        last_name_input = self.browser.find_element_by_name("last_name")
+        iban_input = self.browser.find_element_by_name("iban")
+        first_name_input.clear()
+        first_name_input.send_keys("amir")
+
+        self.browser.find_element_by_name("_save").submit()
+        time.sleep(1)
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('was changed successfully', body.text)
+
+        self.assertIn('amir', body.text)
 
     def tearDown(self):
         time.sleep(1)
